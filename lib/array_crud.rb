cd9cf7a4de81e0c7d8b379267ffb4e9dad31d475
Bobby Neal <rsneal01@gmail.com>
2020-04-24 16:24:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -33,7 +33,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  lab_array = ["wow!",1,2,3,4,"arrays!"]
 end
 
 def retrieve_last_element_from_array(array)
